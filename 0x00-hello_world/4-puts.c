4101c1ce11ad18cce7688c2225cac81ab58b8c39
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-15 06:46:56 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*this is the main fuction*/
+int main(void)
+{
+	char *calling = "Programming is like building a multilingual puzzle";
+
+
+        puts(calling);
+
+	return (0);
+}
