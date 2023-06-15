7562e6991a9995161d633d1853ce81b6336f36d5
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-15 06:58:14 -0700

    4-outS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /*this is the main fuction*/
 int main(void)
 {
-	char *calling = "Programming is like building a multilingual puzzle";
+	char *calling = "'Programming is like building a multilingual puzzle";
 
 
-        puts(calling);
+	puts(calling);
 
 	return (0);
 }
