e4e2ba1a97d1accb444d8d36b99b8387d6466aaa
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-16 10:11:45 -0700

    comb.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,10 @@ int main(void)
 	for (x = 0; x < 10; x++)
 	{
 		putchar(x + '0');
-			if
-				(x < 9);
-				putchar(',');
-				putchar(' ');
+		if
+		(x < 9)
+		putchar(',');
+		putchar(' ');
 	}
 	return (0);
 }
