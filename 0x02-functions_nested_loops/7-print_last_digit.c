615c9546c225e180078b6210cd1963c2d83cc185
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-20 07:15:27 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - to print out the last digit
+ * @n: interger to be checked
+ * Return: n the value of last digit
+ */
+int print_last_digit(int n)
+{
+	 n = n % 10;
+
+	return (n);
+}
