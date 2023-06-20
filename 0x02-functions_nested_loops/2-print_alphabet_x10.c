feb34b6a2fb5f0bef096cb7246b48ca2dd158239
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-20 05:05:33 -0700

    printing alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets ten times
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int times;
+
+	char alpha;
+
+	for (times = 0; times < 10; ++times)
+		for (alpha = 'a'; alpha <= 'z'; ++alpha)
+		_putchar(alpha);
+}
