1a5967e471d942bb4e0f7bdecf458829488c998e
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-20 08:18:22 -0700

    last print

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,20 @@
 /**
  * print_last_digit - to print out the last digit
  * @n: interger to be checked
- * Return: n the value of last digit
+ * Return: n the value ofwhere we  exract from
  */
 int print_last_digit(int n)
 {
-	 n = n % 10;
+	 int a;
 
-	return (n);
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+	return (a);
 }
