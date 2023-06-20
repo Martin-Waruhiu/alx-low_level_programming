88b20e4660b178cf4fa713b189128cc365c65073
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-20 06:58:05 -0700

    checking absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - check absolute value
+ * @n: integer to check
+ * Return: n if positive and -n otherwise
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
