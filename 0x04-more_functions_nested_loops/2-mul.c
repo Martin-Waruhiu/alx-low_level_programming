103e26b15d68c4736d4471ff883468facaa474b9
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-22 00:20:09 -0700

    multiplying integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - to multiply numbers
+ * @a: - inter to multiply
+ * @b: - interger to multiply with
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	result = a * b;
+
+	_putchar(result);
+
+	return (0);
+}
