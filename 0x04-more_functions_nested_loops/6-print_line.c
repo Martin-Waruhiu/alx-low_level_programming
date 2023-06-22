4c36fefdd0f4a0007fe1ec8f92c21276d13ab273
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-22 03:38:01 -0700

    printlines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,18 +9,18 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
 
-	int i;
+		int i;
 
-	for (i = 0; i < n; i++)
-	{
+		for (i = 0; i < n; i++)
+		{
 		_putchar('_');
 		_putchar('\n');
-	}
+		}
 	}
 	_putchar('\n');
 }
