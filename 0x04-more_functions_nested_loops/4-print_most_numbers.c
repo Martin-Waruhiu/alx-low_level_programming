ef5fd11a749157993dc33c51c0eb3293e4101373
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-22 01:07:59 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints digits apart from 2 and 4
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char most_numbers;
+
+	for (most_numbers = '0'; most_numbers <= '9'; most_numbers++)
+	{
+		if (most_numbers != '2' && most_numbers != '4')
+		_putchar(most_numbers);
+	}
+	_putchar('\n');
+}
