c7a03dbfd4e0ea71bc0bb45c2df07b9b97d387f2
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-22 03:23:45 -0700

    printiing a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - function to print line
+ *@n: integer to be checked
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+		_putchar('\n');
+	if (n <= 0)
+	_putchar('\n');
+	}
+}
