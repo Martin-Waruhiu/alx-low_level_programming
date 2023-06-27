9265b2fa1f7aa790eb253418be71261072a2e508
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-27 11:10:13 -0700

    sring to intT

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,50 +1,46 @@
 #include "main.h"
 
 /**
- * _atoi - function to convert string to interger
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * @s: - pointerto cahr
- * Return: n
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int i = 0;
+	int i, d, n, len, f, digit;
 
-	int d = 0;
-
-	int len = 0;
-
-	int n = 0;
-
-	int f = 0;
-
-	int digit = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-
 			if (d % 2)
-			digit = -digit;
-
+				digit = -digit;
 			n = n * 10 + digit;
-
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-			break;
+				break;
 			f = 0;
 		}
+		i++;
 	}
+
 	if (f == 0)
 		return (0);
 
 	return (n);
 }
-
