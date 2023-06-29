8191650f731ed09b1034943bbbfd8301f771fe48
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-06-29 02:47:36 -0700

    concatenate string condition n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncat - function to concatenate
+ * @dest: - destination
+ * @src: - source
+ * @n: - number of bytes allowed for source files
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		if (*src <= n)
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	if (*src < n)
+		*ptr = '\0';
+	return (dest);
+}
