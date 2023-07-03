cb37564b04386c19255fb5f3f5af9354bfffa43e
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-07-03 02:37:23 -0700

    specific character search

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strchr - function to search for a character
+ *@s: - pointer to string to search
+ *@c: character to search for
+ *Return: pointer to c if found and null if not found
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
