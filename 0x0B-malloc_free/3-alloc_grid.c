a9f659127b5f83a186f8dc14159e7aad1fa22798
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-07-11 11:31:07 -0700

    allocating grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,15 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
  * alloc_grid - function to a 2d array
  * @width: - colums
  * @height: - rows
  * Return: pointer to a 2d array
  */
-
-
-
 int **alloc_grid(int width, int height)
 {
 	int i;
@@ -25,23 +21,23 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-
+	/*allocation memory for each array pointer  that points to the grid*/
 	greed = (int **)malloc(height * sizeof(int *));
-
-
 	if (greed == NULL)
 	{
 		return (NULL);
 	}
-
+	/*allocatin memory for each element*/
 	ptr = (int *)malloc(width * height * sizeof(int));
-
 	if (ptr == NULL)
 	{
 		free(greed);
 		return (NULL);
 	}
-	
+	/**
+	 * assigning the pointers in grid to their specific place
+	 * each row in the arrray
+	 */
 	for (i = 0; i < height; i++)
 	{
 		greed[i] = ptr + (i * width);
@@ -49,10 +45,9 @@ int **alloc_grid(int width, int height)
 		/*initialise each value to zero*/
 		for (j = 0; j < width; j++)
 		{
-		
 			greed[i][j] = 0;
 		}
-		return (greed);
 	}
+	return (greed);
 	return (0);
 }
