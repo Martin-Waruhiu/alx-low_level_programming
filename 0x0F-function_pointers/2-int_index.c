6a53a479d069bac4aec7ca026b258e71877d8dfa
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-07-19 04:08:35 -0700

    integer search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - func to look int
+ * @array: array of elemnts
+ * @size: size of array
+ * @cmp: pointer to func that searches for int
+ * Return: 0 success -1 no success
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int s;
+
+	int store_return;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (s = 0; s  < size; s++)
+	{
+		store_return = (*cmp)(array[s]);
+		if (store_return != 0)
+		{
+		return (s);
+		}
+	}
+	return (-1);
+	_putchar('\n');
+}
+
