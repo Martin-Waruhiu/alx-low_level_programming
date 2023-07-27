7d7e316b4b16c08d3ccd6de332c8ca98149031dc
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-07-27 07:12:33 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_list - writes node elements
- * @h:pointer tofirst node
- * Return: size
+ * print_list - writes the members of listt
+ * @h: pointer tomlist to print
+ *
+ * Return:nodes
  */
 size_t print_list(const list_t *h)
 {
+size_t spp = 0;
 
-	size_t node = 0;
 
-	/*h points to the whole  first node*/
 while (h)
 {
 if (!h->str)
 printf("[0] (nil)\n");
 else
-printf("[%u]%s\n", h->len, h->str);
-h = h->next;/*let h point to next node*/
-node++;
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+spp++;
 }
-return (node);
+return (spp);
 }
