d0ddb4bb36e2935a0082f7c063567e074e586656
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-08-01 06:34:49 -0700

    return nth node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * get_nodeint_at_index - function to fin nth node
+ * @head: pointer to ffirst node
+ * @index: index to nth node
+ * Return: nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tempo, *ptr;
+
+	unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	tempo = head;
+	while (i != index)
+	{
+		tempo = tempo->next;
+		i++;
+	}
+	ptr = tempo->next;
+
+	return (ptr);
+}
