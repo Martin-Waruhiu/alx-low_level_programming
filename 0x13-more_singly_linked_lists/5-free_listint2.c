9b8d2249474e6b110ca22aa87a2b2c8e2707e382
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-08-01 06:09:02 -0700

    freee list set to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free lists
+ * @head: pointer to fisrt node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptrhead, *p;
+
+	p = *head;
+	while (p)
+	{
+		ptrhead = p->next; /*store next node temoraririly*/
+		free(p);  /*free the current*/
+		p = ptrhead; /*move to the next node*/
+	}
+	*head = NULL;
+}
