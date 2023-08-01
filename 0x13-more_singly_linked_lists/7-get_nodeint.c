f0d3d37071947118866701278914bbe0a38126be
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-08-01 06:47:23 -0700

    return nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,7 +20,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	}
 	tempo = head;
-	while (i != index)
+	while (i != index && tempo)
 	{
 		tempo = tempo->next;
 		i++;
