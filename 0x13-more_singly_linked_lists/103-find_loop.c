89f3648f1c0e3c25f49d9669bf67f5ad8f8b8ea8
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-08-02 13:08:31 -0700

    find loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+
+/**
+ * find_listint_loop - to find olop
+ * @head: ptr
+ * Return:0
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *s = head; /*s is slow f is fast*/
+	listint_t *f = head;
+
+
+	if (!head)
+		return (NULL);
+
+
+	while (s && f  && f->next)
+	{
+		f = f->next->next;
+		s = s->next;
+		if (f == s)
+		{
+			s = head;
+			while (s != f)
+			{
+				s = s->next;
+				f = f->next;
+			}
+			return (f);
+		}
+	}
+
+
+	return (NULL);
+}
