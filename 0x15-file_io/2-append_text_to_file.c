e2dd9425ccfeae5403c37aa2034adb1c9b731aa4
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-08-07 14:48:36 -0700

    appendind

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,14 +9,14 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int x, fd, length;
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
 			length++;
-	}
-	if (filename == NULL)
-	{
-		return (-1);
 	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	x = write(fd, text_content, length);
