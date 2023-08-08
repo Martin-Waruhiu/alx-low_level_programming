d904bc10a4c1e88e8d0dee6df8447d2b0ac5c01b
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-08-08 00:55:12 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,79 +3,96 @@
 #include <stdlib.h>
 
 char *create_buffer(char *filename);
-void closing_file(int fd);
-/**
- * main - entry point
- * @argc: argument count
- * @argv: argument vector
- * Return: 0
- */
-int main(int argc, char *argv[])
-{
-	int fp1, fp2, x, k;
-	char *buffer;
+void close_file(int fd);
 
-	if (argc != 3)
-	{
-		exit(97);
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	}
-	buffer = create_buffer(argv[2]);
-	fp1 = open(argv[1], O_RDONLY);
-	x = read(fp1, buffer, 1024);
-	fp2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	do {
-
-		if (fp1 == -1 || x == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-		k = write(fp2, buffer, 1024);
-		if (fp2 == -1 || k == -1)
-		{
-			dprintf(STDERR_FILENO, "Error:  Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
-		x = read(fp1, buffer, 1024);
-		fp2 = open(argv[2], O_WRONLY | O_APPEND);
-	} while (x > 0);
-	free(buffer);
-	closing_file(fp1);
-	closing_file(fp2);
-	return (0);
-}
 /**
- * create_buffer - func to create buffer
- * @filename: name of file
- * Return: buffer
+ * create_buffer - Allocate memoery for buffer.
+ * @filename:name of file
+ *
+ * Return: ptr to buffer
  */
 char *create_buffer(char *filename)
 {
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * 1024);
+
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", filename);
 		exit(99);
 	}
+
 	return (buffer);
 }
+
 /**
- * closing_file - closes file
- * @fd: - file descriptor
+ * closing_file - Close f'ds
+ * @fd: The file descriptor
  */
 void closing_file(int fd)
 {
-	int p;
+	int d;
 
-	p = close(fd);
-	if (p == -1)
+	d = close(fd);
+
+	if (d == -1)
 	{
-		dprintf(STDERR_FILENO,  "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
+}
+
+/**
+ * main - Copy f1 to file 2
+ * @argc: argz count
+ * @argv: argv vector
+ *
+ * Return: 0 success -1 fail
+ */
+int main(int argc, char *argv[])
+{
+	int fp1, fp2, j, k;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	fp1 = open(argv[1], O_RDONLY);
+	j = read(fp1, buffer, 1024);
+	fp2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (fp1 == -1 || j == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		k = write(fp2, buffer, j);
+		if (fp2 == -1 || k == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		j = read(fp1, buffer, 1024);
+		fp2 = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (j > 0);
+
+	free(buffer);
+	closing_file(fp1);
+	closing_file(fp2);
+
+	return (0);
 }
