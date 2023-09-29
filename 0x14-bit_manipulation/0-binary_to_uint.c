352328b187d370f6990fb69bb9894c2063932aff
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-09-28 19:05:54 -0700

    c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-        int x;
-        unsigned int rslt = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-        if (!b)
-                return (0);
+	if (!b)
+		return (0);
 
-        for (x = 0; b[x]; x++)
-        {
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-                if (b[x] < '0' || b[x] > '1')
-                {
-                        return (0);
-                }
-        rslt = 2 * rslt + (b[x] - '0');
-        }
-        return (rslt);
+	return (dec_val);
 }
+
